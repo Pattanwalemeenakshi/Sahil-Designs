d2581034569db62a4eca9408675ce53b02458e0d
MEENAKSHI <168412587+Pattanwalemeenakshi@users.noreply.github.com>
2026-05-07 17:20:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 56; i++) {
 
     const img = document.createElement("img");
 
-    img.src = "images/design" + i + ".jpg.jpeg";
+    img.src = "images/design" + i + ".jpg.jpeg"; 
 
     img.alt = "Design " + i;
 
